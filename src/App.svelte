24cc5ae10f9fce599a277bdcf80b3561f3b9e305
whitebackdoor <whitebackdoor@gmail.com>
2023-01-10 19:41:56 +0100

    Configure yahoo api connection

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,29 @@
 <script>
+  import { onMount } from 'svelte';
 
+  let summaryDetail = null;
+  
+  onMount(async () => {
+		const res = await fetch('/api/v11/finance/quoteSummary/AAPL?modules=summaryDetail');
+    const resBody = await res.json();
+
+    summaryDetail = resBody.quoteSummary.result[0].summaryDetail;
+
+    console.log(summaryDetail);
+	});
 </script>
 
 <main>
-  
+  {#if summaryDetail}
+    <div>
+      {`$${summaryDetail.previousClose.fmt}`}
+    </div>
+    <div>
+      {`$${summaryDetail.dividendRate.fmt}`}
+    </div>
+  {/if}
 </main>
 
 <style>
- 
+
 </style>
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  server: {
+    proxy: {
+      '/api': {
+           target: 'https://query2.finance.yahoo.com',
+           changeOrigin: true,
+           secure: false,
+           rewrite: (path) => path.replace(/^\/api/, ""),  
+       }
+    }
+  }
 })
