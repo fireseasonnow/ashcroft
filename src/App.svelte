4f6be9f23b3b1018127d6da2bb3fd3d730be61ed
whitebackdoor <whitebackdoor@gmail.com>
2023-01-13 17:46:26 +0100

    Refactor formatValueToPrice()

--- src/App.svelte
+++ src/App.svelte
@@ -1,40 +1,36 @@
 <script>
   import { onMount } from "svelte";
 
-  let summaryDetail = {};
+  let summaryDetail;
 
-  $: ({ previousClose, dividendRate, dividendYield } = summaryDetail);
+  $: ({ previousClose, dividendRate, dividendYield } = summaryDetail || {});
 
   onMount(async () => {
-    const res = await fetch(
-      "/api/v11/finance/quoteSummary/AAPL?modules=summaryDetail"
-    );
-    const resBody = await res.json();
+    try {
+      const res = await fetch(
+        "/api/v11/finance/quoteSummary/AAPL?modules=summaryDetail"
+      );
+      const resBody = await res.json();
 
-    summaryDetail = resBody.quoteSummary.result[0].summaryDetail;
-
-    console.log(summaryDetail);
+      summaryDetail = resBody.quoteSummary.result[0].summaryDetail;
+    } catch (error) {
+      console.error(`Error getting quote summary: ${error}`);
+    }
   });
 
-  const calculatePrice = ({ raw }, times) => {
-    let withCommas = (raw * times).toLocaleString();
-    const split = withCommas.split(".");
-    let final = withCommas;
+  const formatValueToPrice = (value, factor) => {
+    const sum = value.raw * factor;
 
-    if (split.length === 1) {
-      final += ".00";
-    }
-
-    if (split.length === 2 && split[1].length === 1) {
-      final += "0";
-    }
-
-    return `$${final}`;
+    return sum.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2,
+    });
   };
 </script>
 
 <main>
-  {#if Object.keys(summaryDetail).length}
+  {#if summaryDetail}
     <h1>Apple (AAPL) {new Date().toLocaleDateString()}</h1>
     <div>
       Previous Close Price
@@ -42,37 +38,37 @@
       <ul>
         <li>
           10 shares -
-          {`${calculatePrice(previousClose, 10)}`}
+          {formatValueToPrice(previousClose, 10)}
         </li>
         <li>
           100 shares -
-          {`${calculatePrice(previousClose, 100)}`}
+          {formatValueToPrice(previousClose, 100)}
         </li>
         <li>
           1000 shares -
-          {`${calculatePrice(previousClose, 1000)}`}
+          {formatValueToPrice(previousClose, 1000)}
         </li>
       </ul>
     </div>
     <div>
       Dividend Rate Quarterly
-      {`$${summaryDetail.dividendRate.fmt}`}
+      {`$${dividendRate.fmt}`}
     </div>
     <div>
       Dividend Rate Yearly
-      {`${calculatePrice(dividendRate, 4)}`}
+      {formatValueToPrice(dividendRate, 4)}
       <ul>
         <li>
           10 shares -
-          {`${calculatePrice(dividendRate, 40)}`}
+          {formatValueToPrice(dividendRate, 40)}
         </li>
         <li>
           100 shares -
-          {`${calculatePrice(dividendRate, 400)}`}
+          {formatValueToPrice(dividendRate, 400)}
         </li>
         <li>
           1000 shares -
-          {`${calculatePrice(dividendRate, 4000)}`}
+          {formatValueToPrice(dividendRate, 4000)}
         </li>
       </ul>
     </div>
